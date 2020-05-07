81e2dbd7eee00fd9c5159a647c61b551340d5c6e
Rina <rinamyriamh@gmail.com>
2020-05-07 18:04:20 +0200

    modif de don

--- vues/vCarteBancaire.php
+++ vues/vCarteBancaire.php
@@ -1,1 +1,68 @@
-<p>Vues</p>
+
+  <main id="main">
+
+    <!-- ======= Breadcrumbs ======= -->
+    <section id="breadcrumbs" class="breadcrumbs">
+      <div class="container">
+
+        <ol>
+          <li><a href="Accueil.php">Accueil</a></li>
+          <li>Faire Un Don</li>
+        </ol>
+        <h2>Faire un Don</h2>
+
+      </div>
+    </section><!-- End Breadcrumbs -->
+
+    <!-- ======= Contact Section ======= -->
+    <section id="contact" class="contact">
+      <div class="container">
+
+
+        <div class="row">
+          <div class="col-lg-12">
+              <p>À travers votre don vous participez à apporter les fonds pour aider le Beth Habbad de l'Essone. Différents moyens de paiements sont mis à votre disposition.</p><br>
+
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-4 col-md-6">
+            <div class="info-box mb-4">
+              <i class="bx bx-map"></i>
+              <h3>Don par Allodons</h3>
+              <p><a href="http://allodons.fr/Beth-Loubavitch-Essonne-Sud"><img src="assets/img/allodonslogo.jpg"></a></p>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6">
+            <div class="info-box mb-4">
+              <i class="bx bx-map"></i>
+              <h3>Don par Paypal</h3>
+              <p><a href="http://allodons.fr/Beth-Loubavitch-Essonne-Sud"><img src="assets/img/paypallogo.png"></a></p>
+            </div>
+          </div>
+
+          	<div class="col-lg-4 col-md-6">
+            <div class="info-box md-4">
+              <i class="bx bx-phone-call"></i>
+              <h3>Par Chèque</h3>
+              <p style="padding-left: 12px; padding-right: 12px; font-size: 18px;padding-bottom: 10px;">Si vous souhaitez envoyer un chèque par voie postale veuillez nous contacter au :<br/> <a href="tel://23923929210">+33650242463</a></p>
+            </div>
+          </div>
+          </div>
+          <div class="row" style="margin-top: 20px;">
+          	<div class="col-lg-4">
+          	</div>
+          	<div class="col-lg-6">
+          		<p style="font-size: 18px;">Le Beth Habbad d'Essonne vous remercie.</p>
+          	</div>
+          	
+          </div>
+        
+
+      </div>
+    </section><!-- End Contact Section -->
+
+  </main><!-- End #main -->
+
+
