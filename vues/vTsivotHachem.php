b154dfa044414dbf45ff51af3feed7bf818fa047
CindyLevy <64846774+CindyLevy@users.noreply.github.com>
2020-05-12 23:10:55 +0200

    Update and rename vues/vTsivotHachem.php to  vues/vTsivotHachem.php

--- vues/vTsivotHachem.php
+++ vues/vTsivotHachem.php
@@ -1,1 +1,73 @@
-<p>Vues</p>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Projet Beth Habbad</title>
+  <meta content="" name="descriptison">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Eterna - v2.0.0
+  * Template URL: https://bootstrapmade.com/eterna-free-multipurpose-bootstrap-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+<div class="container">
+	<div class="row">
+		<!-- Affiches -->
+		<div class="col-lg-6">
+    	<div class="text-center">
+ 				<br><br><h2> Prochaine activité Tsivot Hachem de Ris Orangis :</h2>
+        <p> Rendez vous lundi 11 mai 2020 à  18h00 à la Synaguogue d'Evry</p>
+     	</div>
+      <div class="text-center">
+			 <img src="assets/img/Affiches/WhatsApp Image 2020-05-04 at 19.21.15.jpeg" class="rounded" height="650" alt="Erreur">
+			</div>
+      <br>
+		</div>	
+
+		<div class="col-lg-6">
+			<!-- Image Tsivots Hachem -->
+			<br><br><br>
+			<img src="assets/img/image BethHabbad/Logo-Tsivot-Hachem.png" align="right" height="200">
+			<B> Qu'est ce que Tsivot Hachem ?</B>
+			<p class="text-justify"><br> Tsivot Hachem, l'armée d'Hachem, est l'une des nombreuses institutions internationale fondée par le Rabbi de Loubavitch, avec des filiales dans chaque continents et des centaines de milliers de membres : de partout, les enfants sont appelés à se joindre à cette armée merveilleuse, et trés spéciale! <B> D'ailleurs, le Beth Habad de Ris Oranis organise régulièrement des activités Tsivot Hachem ! </B>  Vous, enfants, êtes les soldats et les officiers, et Hachem Lui-même est le commandant en chef. C'est pourquoi on l'appelle Tsivot Hachem. Tsivot Hachem est une force dynamique réunissant les enfants Juifs depuis la naissance jusqu'à la Bar ou Bat Mitsva. Le but de Tsivot Hachem est de faire vivre à vos enfants le judaïsme à travers nos clubs, nos rassemblements, nos publications. La participation aux activités et la pratique de bonnes actions leur apportera des récompenses. Vos enfants seront alors imprégnés d'amour et de connaissance du judaïsme.<br><br> En savoir plus sur <a href="https://tsivothachem.fr/">tsivothachem.fr</a> <br> Mme Sebbag: <i class="contact-info mr-auto">06 51 79 53 07</i></p>
+			<B> Tsivots Hachem, c'est aussi... </B> 
+      <p></p>
+			<p class="text-justify"><i class="icofont-check-circled"></i> Ateliers itinérants </p>
+			<p class="text-justify"><i class="icofont-check-circled"></i> Concours récompensés, pour tous niveaux </p>
+			<p class="text-justify"><i class="icofont-check-circled"></i> Club anniversaire </p>
+			<p class="text-justify"><i class="icofont-check-circled"></i> Journée d'animations </p>
+			<p class="text-justify"><i class="icofont-check-circled"></i> Kids Magasine </p>
+			<p class="text-justify"><i class="icofont-check-circled"></i> Et encore pleins d'autres surprises ! </p>
+		</div>
+	</div>
+</div>
+
+</body>
