dc84c521fdc6209084e266a3f1ff0ff124eab214
CindyLevy <64846774+CindyLevy@users.noreply.github.com>
2020-05-12 22:51:29 +0200

    Update and rename vues/vCoursHebdo.php to  vues/vCoursHebdo.php

--- vues/vCoursHebdo.php
+++ vues/vCoursHebdo.php
@@ -1,38 +1,49 @@
+<!DOCTYPE html>
+<html>
+<title>W3.CSS</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<body>
 
 <div class="container">
+  
+  <br>
 <div style="text-align:center">
-<h1>Cours Hebdomadaires</h1>
+  <h2 class="display-4" >Cours hebdomadaires</h2> <br>
 </div>
 
 <table class="table table-dark" style="text-align:center">
   <thead>
     <tr>
-      <th scope="col">Nom du Rav</th>
+      <th scope="col">Conférencier</th>
       <th scope="col">Lieu</th>
-      <th scope="col">Horraire</th>
-      <th scope="col">Sujet</th>
+      <th scope="col">Date et horaire</th>
+      <th scope="col">Description</th>
     </tr>
   </thead>
-  <tbody>
-    <tr>
-      <td>Mark</td>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
+  <tbody><tr>
+      <td>Rav Cohen </td>
+      <td>18 Quai de la Borde 91130 Ris Orangis</td>
+      <td>Tous les lundi soir à 19h30</td>
+      <td>Cour pour hommes</td>
     </tr>
     <tr>
-      <td>Jacob</td>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
+      <td>Rav Sebbag </td>
+      <td>18 Quai de la Borde 91130 Ris Orangis</td>
+      <td>Tous les mardis soir à 20h</td>
+      <td>Cour pour hommes</td>
     </tr>
     <tr>
-      <td>Larry</td>
-      <td>Larry</td>
-      <td>the Bird</td>
-      <td>@twitter</td>
+      <td>Mme Haya Sebbag </td>
+      <td>18 Quai de la Borde 91130 Ris Orangis</td>
+      <td>Tous les dimanche soir à 20h30</td>
+      <td>Cour pour femmes</td>
     </tr>
+    
+    
   </tbody>
 </table>
 
 </div>
+
+</body>
