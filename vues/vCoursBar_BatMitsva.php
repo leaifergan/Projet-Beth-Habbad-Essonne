0f82c5dd87d4ca4d9fb3122c2985b3d7b7ee4b3a
CindyLevy <64846774+CindyLevy@users.noreply.github.com>
2020-05-12 22:48:01 +0200

    Update and rename vues/vCoursBar_BatMitsva.php to  vues/vCoursBar_BatMitsva.php

--- vues/vCoursBar_BatMitsva.php
+++ vues/vCoursBar_BatMitsva.php
@@ -1,38 +1,66 @@
 
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Projet Beth Habbad</title>
+  <meta content="" name="descriptison">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Eterna - v2.0.0
+  * Template URL: https://bootstrapmade.com/eterna-free-multipurpose-bootstrap-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
 <div class="container">
-<div style="text-align:center">
-<h1>Cours Hebdomadaires</h1>
+  <div class="row">
+    <!-- Affiches -->
+    <div class="col-lg-6">
+      <div class="text-center">
+        <br><br><h2 style="background: #CEE3EA"><i> Bar Mitsva </i>- בר מצווה  </h2>
+        <img src="assets/img/image BethHabbad/MazalTovG.png" align="left">
+        <p class="text-justify"> <br> <li class="text-justify">Lorsqu'un garçon atteint l'âge de 13 ans, il devient Bar Mitsva. </li><li class="text-justify"> A partir de ce jour, ils ont l'obligation de faire toutes les Mitsvots et sont considérés par la Torah comme des adultes, c'est-à-dire qu'ils sont, dés ce moment, responsables de leurs actes et doivent donc respecter scupuleusement les commandements de la Torah, positifs et négatifs. </li><li class="text-justify"> Un garçon Bar Mitsva est appelé à la Torah (Aliya) pour la première fois; il met les Téfilines chaque jours et il compte maintenant dans un Minyane et peut même conduire la prière.</li><li class="text-justify">On organise un repas de fête avec la famille et les amis, au cours duquel le jeune Bar Mitsva fait un discours sur la Paracha de la semaine ou l'importance des Téfilines. Mais l'essentiel de la célébration est que le jeune garçon s'engage maintenant à faire exactement toutes les Mitsvots d'Hachem comme un adulte.</li> <li><B>Le Beth Habad de Ris Orangis propose des cours pré-BarMitsva pour vous aider à préparer ce grand jour !</B></li> <br> 
+        <div class="text-center" style="width: 500px;  padding-top:10px; padding-bottom:10px;border: 3px solid #A0A0A0; text-align: center;background: #C0C0C0;">
+          <p>En savoir plus sur <a href="https://fr.chabad.org/library/article_cdo/aid/2527842/jewish/Les-lois-de-la-Bar-Mitsva.htm">chabbad.org</a><br> Rav Sebbag: <i class="contact-info mr-auto">06 50 24 24 63</i></p>
+        </div><br>
+      </div>
+    </div>
+    <div class="col-lg-6">
+      <div class="text-center">
+        <br><br><h2 style="background: #F1B4D5"><i> Bat Mitsva </i> - בת מצווה </h2>
+        <img src="assets/img/image BethHabbad/MazalTovF.png" align="left">
+        <p class="text-justify"> <br><li class="text-justify"> Lorsqu'une fille atteint l'âge de 12 ans, elle devient Bat Mitsva.</li> <li class="text-justify">A partir de ce jour, elles ont l'obligation de faire toutes les Mitsvots et sont considérées par la Torah comme des adultes, c'est-à-dire qu'elles sont, dés ce moment, responsables de leurs actes et doivent donc respecter scupuleusement les commandements de la Torah, positifs et négatifs.</li> <li class="text-justify">Une Bat Mitsva organise un repas de fête avec ses amies proches, elle fait un petit discours et prend la bonne résolution d'accomplir toutes les Misvots. </li> <li class="text-justify">Les mois qui précèdent la Bat Mitsva sont un bon moment pour en apprendre davantage sur les mitsvot et sur le comment et le pourquoi de leur accomplissement, et permettra de bien préparer cette nouvelle vie. C’est une période idéale pour développer votre propre relation personnelle avec le judaïsme et avec D.ieu. </li> <li><B>Le Beth Habad de Ris Orangis propose des cours pré-BatMitsva pour vous aider à préparer ce grand jour !</B></li> <br> 
+        <div class="text-center" style="width: 500px;  padding-top:10px; padding-bottom:10px;border: 3px solid #A0A0A0; text-align: center;background: #C0C0C0;">
+          <p>En savoir plus sur <a href="https://fr.chabad.org/library/article_cdo/aid/2763303/jewish/La-Bat-Mitsva.htm">chabbad.org</a><br> Mme Sebbag: <i class="contact-info mr-auto">06 51 79 53 07</i></p> 
+        </div>
+      </div><br>
+    </div>
+  </div>
 </div>
 
-<table class="table table-dark" style="text-align:center">
-  <thead>
-    <tr>
-      <th scope="col">Nom du Rav</th>
-      <th scope="col">Lieu</th>
-      <th scope="col">Horraire</th>
-      <th scope="col">Sujet</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>Mark</td>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <td>Jacob</td>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <td>Larry</td>
-      <td>Larry</td>
-      <td>the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-</table>
-
-</div>
+</body>
