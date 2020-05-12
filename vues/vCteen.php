14426a7258e0f0efaec89669b60a53fce3d8b3b8
CindyLevy <64846774+CindyLevy@users.noreply.github.com>
2020-05-12 22:53:37 +0200

    Update and rename vues/vCteen.php to  vues/vCteen.php

--- vues/vCteen.php
+++ vues/vCteen.php
@@ -1,1 +1,72 @@
-<p>Vues</p>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Projet Beth Habbad</title>
+  <meta content="" name="descriptison">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Eterna - v2.0.0
+  * Template URL: https://bootstrapmade.com/eterna-free-multipurpose-bootstrap-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+<div class="container">
+	<div class="row">
+		<!-- Affiches -->
+		<div class="col-lg-6">
+    	<div class="text-center">
+ 				<br><br><h2> Prochaine activité Cteen de Ris Orangis :</h2>
+        <p> Rendez vous lundi 11 mai 2020 à  18h00 à la Synaguogue d'Evry</p>
+     	</div>
+      <div class="text-center">
+			 <img src="assets/img/Affiches/WhatsApp Image 2020-05-04 at 19.14.27.jpeg" class="rounded" height="650" alt="Erreur">
+			</div>
+      <br>
+		</div>	
+
+		<div class="col-lg-6">
+			<!-- Image Tsivots Hachem -->
+			<br><br><br>
+			<img src="assets/img/image BethHabbad/Logo-cteen.png" align="right" height="200">
+			<B> Qu'est ce que Cteen?</B>
+			<p class="text-justify"><br> Le mouvement Cteen est déstiné à tous jeunes adolescants Juifs. D'origine américaine, le mouvement Cteen s'est répandu dans le monde entier jusqu'à Evry! Cteen te propose d'assister régulièrement aux activités qu'ils proposent : Patinoire Cteen géante, allumage sur la Seine pour Hannoucca, voyage organisé à New York... En à peine cinq ans, le mouvement CTeen attire des dizaines de milliers de jeunes Juifs ! <br><br> En savoir plus sur <a href="https://www.cteen.fr/">cteen.fr</a> <br> Mme Sebbag: <i class="contact-info mr-auto">06 51 79 53 07</i></p>
+			<B> Cteen, c'est aussi... </B> 
+      <p></p>
+			<p class="text-justify"><i class="icofont-check-circled"></i> L'occasion de faire pleins de nouvelles rencontres</p>
+			<p class="text-justify"><i class="icofont-check-circled"></i> Renforcer son judaïsme </p>
+			<p class="text-justify"><i class="icofont-check-circled"></i> Des voyages organsiés et Chabbats pleins  </p>
+		</div>
+	</div>
+</div>
+<img src="assets/img/slide/CteenOng.jpg">
+
+
+</body>
