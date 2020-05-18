fb09c55550e0913597c3c577d12957a28b955558
CindyLevy <64846774+CindyLevy@users.noreply.github.com>
2020-05-18 15:08:26 +0200

    Update vAccueil.php

--- vues/vAccueil.php
+++ vues/vAccueil.php
@@ -3,13 +3,10 @@
   <section id="hero">
     <div class="hero-container">
       <div id="heroCarousel" class="carousel slide carousel-fade" data-ride="carousel">
-
         <ol class="carousel-indicators" id="hero-carousel-indicators"></ol>
-
         <div class="carousel-inner" role="listbox">
-
           <!-- Slide 1 -->
-          <div class="carousel-item active" style="background: url(assets/img/slide/Rabbi.jpg)">
+          <div class="carousel-item active" style="background: url(assets/img/Rabbi.jpg)">
             <div class="carousel-container">
               <div class="carousel-content"> 
                 <h2 class="animated fadeInDown">Beth Habad <span>Essonne Sud</span></h2>
@@ -20,9 +17,8 @@
               </div>
             </div>
           </div>
-
           <!-- Slide 2 -->
-          <div class="carousel-item" style="background: url(assets/img/slide/Cteen.jpg)">
+          <div class="carousel-item" style="background: url(assets/img/Cteen.jpg)">
             <div class="carousel-container">
               <div class="carousel-content">
                 <h2 class="animated fadeInDown"> Rejoinds <span>Cteen Essonne Sud !</span></h2>
@@ -31,9 +27,8 @@
               </div>
             </div>
           </div>
-
           <!-- Slide 3 -->
-          <div class="carousel-item" style="background: url(assets/img/slide/slide-3.jpg)">
+          <div class="carousel-item" style="background: url(assets/img/slide-3.jpg)">
             <div class="carousel-container">
               <div class="carousel-content">
                 <h2 class="animated fadeInDown">Sequi ea <span>Dime Lara</span></h2>
@@ -43,27 +38,22 @@
             </div>
           </div>
         </div>
-
         <a class="carousel-control-prev" href="#heroCarousel" role="button" data-slide="prev">
           <span class="carousel-control-prev-icon icofont-rounded-left" aria-hidden="true"></span>
           <span class="sr-only">Previous</span>
         </a>
-
         <a class="carousel-control-next" href="#heroCarousel" role="button" data-slide="next">
           <span class="carousel-control-next-icon icofont-rounded-right" aria-hidden="true"></span>
           <span class="sr-only">Next</span>
         </a>
-
       </div>
     </div>
   </section><!-- End Hero -->
 
   <main id="main">
-
     <!-- ======= Featured Section ======= -->
  <!--   <section id="featured" class="featured">
       <div class="container">
-
         <div class="row">
           <div class="col-lg-4">
             <div class="icon-box">
@@ -87,14 +77,11 @@
             </div>
           </div>
         </div>
-
       </div>  -->
   <!--  </section> End Featured Section --> 
-
     <!-- ======= About Section ======= 
     <section id="about" class="about">
       <div class="container">
-
         <div class="row">
           <div class="col-lg-6">
             <img src="assets/img/about.jpg" class="img-fluid" alt="">
@@ -117,10 +104,8 @@
             </p>
           </div>
         </div>
-
       </div>-->
 <!--     </section>End About Section -->
-
 <section id="portfolio" class="portfolio">
       <div class="container">
         <br>
@@ -138,148 +123,133 @@
             </ul>
           </div>
         </div>
-
         <div class="row portfolio-container">
-
           <div class="col-lg-4 col-md-6 portfolio-item filter-etudiants">
             <div class="portfolio-wrap">
-              <img src="assets/img/portfolio/Téfiline.jpg" class="img-fluid" alt="">
+              <img src="assets/img/Téfiline.jpg" class="img-fluid" alt="">
               <div class="portfolio-info">
                 <h4>Vérification Téfilin & Mézouzot </h4>
                 <p>Etudiants</p>
                 <div class="portfolio-links">
-                  <a href="assets/img/portfolio/ExplicationTéfiline.jpeg"  class="venobox" title="Vérification Tefilin & Mézouzot"><i class="bx bx-plus"></i></a>
+                  <a href="assets/img/ExplicationTéfiline.jpeg"  class="venobox" title="Vérification Tefilin & Mézouzot"><i class="bx bx-plus"></i></a>
                   <a href="Contact.php" title="Nous Contacter"><i class="bx bx-link"></i></a>
                 </div>
               </div>
             </div>
           </div>
-
           <div class="col-lg-4 col-md-6 portfolio-item filter-fetesjuives">
             <div class="portfolio-wrap">
-              <img src="assets/img/portfolio/Méguila.jpg" class="img-fluid" alt="">
+              <img src="assets/img/Méguila.jpg" class="img-fluid" alt="">
               <div class="portfolio-info">
                 <h4>Pourim: Lectures de Meguila & Distribution de Michloah Manot</h4>
                 <p>Fêtes Juives</p>
                 <div class="portfolio-links">
-                  <a href="assets/img/portfolio/ExplicationMéguila.png" class="venobox" title="Voir Plus"><i class="bx bx-plus"></i></a>
+                  <a href="assets/img/ExplicationMéguila.png" class="venobox" title="Voir Plus"><i class="bx bx-plus"></i></a>
                   <a href="Contact.php" title="Nous Contacter"><i class="bx bx-link"></i></a>
                 </div>
               </div>
             </div>
           </div>
-
           <div class="col-lg-4 col-md-6 portfolio-item filter-fetesjuives">
             <div class="portfolio-wrap">
-              <img src="assets/img/portfolio/Shofar.png" class="img-fluid" alt="">
+              <img src="assets/img/Shoffar.png" class="img-fluid" alt="">
               <div class="portfolio-info">
                 <h4>Roch Hachana: Sonnerie Du Shofar à Domicile</h4>
                 <p>Fêtes Juives</p>
                 <div class="portfolio-links">
-                  <a href="assets/img/portfolio/ExplicationShofar.png" data-gall="portfolioGallery" class="venobox" title="Voir Plus"><i class="bx bx-plus"></i></a>
+                  <a href="assets/img/ExplicationShofar.png" data-gall="portfolioGallery" class="venobox" title="Voir Plus"><i class="bx bx-plus"></i></a>
                   <a href="Contact.php" title="Nous Contacter"><i class="bx bx-link"></i></a>
                 </div>
               </div>
             </div>
           </div>
-
           <div class="col-lg-4 col-md-6 portfolio-item filter-enfantAdos">
             <div class="portfolio-wrap">
-              <img src="assets/img/portfolio/Cteen.png" class="img-fluid" alt="">
+              <img src="assets/img/Cteen.jpeg" class="img-fluid" alt="">
               <div class="portfolio-info">
                 <h4>Cteen</h4>
                 <p>Activité</p>
                 <div class="portfolio-links">
-                  <a href="assets/img/portfolio/portfolio-4.jpg" data-gall="portfolioGallery" class="venobox" title="Voir Plus"><i class="bx bx-plus"></i></a>
+                  <a href="assets/img/portfolio-4.jpg" data-gall="portfolioGallery" class="venobox" title="Voir Plus"><i class="bx bx-plus"></i></a>
                   <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
                 </div>
               </div>
             </div>
           </div>
-
           <div class="col-lg-4 col-md-6 portfolio-item filter-web">
             <div class="portfolio-wrap">
-              <img src="assets/img/portfolio/portfolio-5.jpg" class="img-fluid" alt="">
+              <img src="assets/img/GanIsrael.png" class="img-fluid" alt="">
               <div class="portfolio-info">
                 <h4>Web 2</h4>
                 <p>Web</p>
                 <div class="portfolio-links">
-                  <a href="assets/img/portfolio/portfolio-5.jpg" data-gall="portfolioGallery" class="venobox" title="Web 2"><i class="bx bx-plus"></i></a>
+                  <a href="assets/img/portfolio-5.jpg" data-gall="portfolioGallery" class="venobox" title="Web 2"><i class="bx bx-plus"></i></a>
                   <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
                 </div>
               </div>
             </div>
           </div>
-
           <div class="col-lg-4 col-md-6 portfolio-item filter-app">
             <div class="portfolio-wrap">
-              <img src="assets/img/portfolio/portfolio-6.jpg" class="img-fluid" alt="">
+              <img src="assets/img/Tsivot Hachem.jpg" class="img-fluid" alt="">
               <div class="portfolio-info">
                 <h4>App 3</h4>
                 <p>App</p>
                 <div class="portfolio-links">
-                  <a href="assets/img/portfolio/portfolio-6.jpg" data-gall="portfolioGallery" class="venobox" title="App 3"><i class="bx bx-plus"></i></a>
+                  <a href="assets/img/portfolio-6.jpg" data-gall="portfolioGallery" class="venobox" title="App 3"><i class="bx bx-plus"></i></a>
                   <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
                 </div>
               </div>
             </div>
           </div>
-
           <div class="col-lg-4 col-md-6 portfolio-item filter-card">
             <div class="portfolio-wrap">
-              <img src="assets/img/portfolio/portfolio-7.jpg" class="img-fluid" alt="">
+              <img src="assets/img/portfolio-7.jpg" class="img-fluid" alt="">
               <div class="portfolio-info">
                 <h4>Card 1</h4>
                 <p>Card</p>
                 <div class="portfolio-links">
-                  <a href="assets/img/portfolio/portfolio-7.jpg" data-gall="portfolioGallery" class="venobox" title="Card 1"><i class="bx bx-plus"></i></a>
+                  <a href="assets/img/portfolio-7.jpg" data-gall="portfolioGallery" class="venobox" title="Card 1"><i class="bx bx-plus"></i></a>
                   <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
                 </div>
               </div>
             </div>
           </div>
-
           <div class="col-lg-4 col-md-6 portfolio-item filter-card">
             <div class="portfolio-wrap">
-              <img src="assets/img/portfolio/portfolio-8.jpg" class="img-fluid" alt="">
+              <img src="assets/img/portfolio-8.jpg" class="img-fluid" alt="">
               <div class="portfolio-info">
                 <h4>Card 3</h4>
                 <p>Card</p>
                 <div class="portfolio-links">
-                  <a href="assets/img/portfolio/portfolio-8.jpg" data-gall="portfolioGallery" class="venobox" title="Card 3"><i class="bx bx-plus"></i></a>
+                  <a href="assets/img/portfolio-8.jpg" data-gall="portfolioGallery" class="venobox" title="Card 3"><i class="bx bx-plus"></i></a>
                   <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
                 </div>
               </div>
             </div>
           </div>
-
           <div class="col-lg-4 col-md-6 portfolio-item filter-web">
             <div class="portfolio-wrap">
-              <img src="assets/img/portfolio/portfolio-9.jpg" class="img-fluid" alt="">
+              <img src="assets/img/portfolio-9.jpg" class="img-fluid" alt="">
               <div class="portfolio-info">
                 <h4>Web 3</h4>
                 <p>Web</p>
                 <div class="portfolio-links">
-                  <a href="assets/img/portfolio/portfolio-9.jpg" data-gall="portfolioGallery" class="venobox" title="Web 3"><i class="bx bx-plus"></i></a>
+                  <a href="assets/img/portfolio-9.jpg" data-gall="portfolioGallery" class="venobox" title="Web 3"><i class="bx bx-plus"></i></a>
                   <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
                 </div>
               </div>
             </div>
           </div>
-
         </div>
-
       </div>
     </section><!-- End Portfolio Section -->
-
     <!-- ======= Services Section ======= -->
    <!-- <section id="services" class="services">
       <div class="container">
-
         <div class="section-title" data-aos="fade-up">
           <h2>Our Services</h2>
         </div>
-
         <div class="row">
           <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
             <div class="icon-box">
@@ -288,7 +258,6 @@
               <p>Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi</p>
             </div>
           </div>
-
           <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0">
             <div class="icon-box">
               <div class="icon"><i class="bx bx-file"></i></div>
@@ -296,7 +265,6 @@
               <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore</p>
             </div>
           </div>
-
           <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-lg-0">
             <div class="icon-box">
               <div class="icon"><i class="bx bx-tachometer"></i></div>
@@ -304,7 +272,6 @@
               <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia</p>
             </div>
           </div>
-
           <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
             <div class="icon-box">
               <div class="icon"><i class="bx bx-world"></i></div>
@@ -312,7 +279,6 @@
               <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis</p>
             </div>
           </div>
-
           <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
             <div class="icon-box">
               <div class="icon"><i class="bx bx-slideshow"></i></div>
@@ -320,7 +286,6 @@
               <p>Quis consequatur saepe eligendi voluptatem consequatur dolor consequuntur</p>
             </div>
           </div>
-
           <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
             <div class="icon-box">
               <div class="icon"><i class="bx bx-arch"></i></div>
@@ -328,21 +293,16 @@
               <p>Modi nostrum vel laborum. Porro fugit error sit minus sapiente sit aspernatur</p>
             </div>
           </div>
-
         </div>
-
       </div> -->
   <!--  </section> End Services Section -->
-
     <!-- ======= Clients Section ======= -->
   <!--   <section id="clients" class="clients">
       <div class="container">
-
         <div class="section-title">
           <h2>Clients</h2>
           <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
         </div>
-
         <div class="owl-carousel clients-carousel">
           <img src="assets/img/clients/client-1.png" alt="">
           <img src="assets/img/clients/client-2.png" alt="">
@@ -353,8 +313,6 @@
           <img src="assets/img/clients/client-7.png" alt="">
           <img src="assets/img/clients/client-8.png" alt="">
         </div>
-
       </div>
     </section> --> <!-- End Clients Section -->
-
   </main><!-- End #main -->
