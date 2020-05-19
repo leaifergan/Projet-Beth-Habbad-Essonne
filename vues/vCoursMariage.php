1ff1db17b04349bbbf41fcf86c08e791418ad06e
rina-myriam <57219452+rina-myriam@users.noreply.github.com>
2020-05-19 19:26:42 +0200

    Update vCoursMariage.php

--- vues/vCoursMariage.php
+++ vues/vCoursMariage.php
@@ -1,1 +1,44 @@
-<p>Vues</p>
+
+  <main id="main">
+
+    <!-- ======= Breadcrumbs ======= -->
+    <section id="breadcrumbs" class="breadcrumbs">
+      <div class="container">
+
+        <ol>
+          <li><a href="Accueil.php">Accueil</a></li>
+          <li>Cours de préparation au mariage</li>
+        </ol>
+        <h2>Cours de préparation au mariage</h2>
+
+      </div>
+    </section><!-- End Breadcrumbs -->
+
+    <!-- ======= Contact Section ======= -->
+    <section>
+      <div class="container">
+
+        <div class="row">
+          <div class="col-12">
+              <img src="assets\img\roses.jpg" class="img-fluid" style="max-width: 100%; height: auto">
+
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-12">
+              <p style="margin-top: 2em;">En contactant Mr et Mme SEBAG, vous allez pouvoir acquérir les connaissances pour fonder un foyer de torah.<br>
+              Mme SEBAG : <a href="tel://23923929210">+33651795307</a><br>
+              Mr SEBAG : <a href="tel://23923929210">+33650242463</a>
+              </p>
+
+          </div>
+        </div>
+        
+
+      </div>
+    </section><!-- End Contact Section -->
+
+  </main><!-- End #main -->
+
+
